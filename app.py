1067b99676139e87fdb8f62091624c5186e1a84e
przemo166 <przemek.w.166@gmail.com>
2020-03-25 22:47:56 +0100

    Edited program. Added new features as : easy way to create new frames, opening an app with correct resolution and window title eMenu

--- app.py
+++ app.py
@@ -1,14 +1,48 @@
-# Added a Python package for GUI apps  
-from tkinter import *
+# Imported library for making GUI
+import tkinter as eMenu
 
-# Inicialazing new window
-window = Tk()
+# A global constant for our font type 
+LARGE_FONT= ("Verdana", 12)
 
-# Window title and resolution 
-window.title("Customer app")
-window.geometry('800x480')
+# Main class , everything we have done here is to add new pages in our app
 
-# Main loop 
-window.mainloop()
+class eMenuApp(eMenu.Tk):
 
+    # Initialization
+    def __init__(self, *args, **kwargs):
 
+        eMenu.Tk.__init__(self, *args, **kwargs)
+        container = eMenu.Frame(self)
+
+        container.pack(side="top", fill="both", expand = True)
+
+        container.grid_rowconfigure(0, weight=1)
+        container.grid_columnconfigure(0, weight=1)
+        
+        self.frames = {}
+
+        frame = StartPage(container,self)
+
+        self.frames[StartPage] = frame
+
+        frame.grid(row=0,column=0,sticky="nsew")
+
+        self.show_frame(StartPage)
+
+    def show_frame(self, cont):
+
+        frame = self.frames[cont]
+        frame.tkraise()
+        
+class StartPage(eMenu.Frame):
+
+    def __init__(self, parent, controller):
+        eMenu.Frame.__init__(self,parent)
+        label = eMenu.Label(self, text="Start Page", font=LARGE_FONT)
+        label.pack(pady=10,padx=10)
+
+app = eMenuApp()
+app.title('eMenu')
+app.geometry('800x480')
+
+app.mainloop()
